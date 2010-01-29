23e2201f6295ba93e61649f56420c1ff6b459a0e
Alexander Polakov <polachok@gmail.com>
2010-01-30 01:37:10 +0300

    add parser

--- iriq.py
+++ iriq.py
@@ -7,27 +7,42 @@ pygtk.require('2.0')
 import gtk
 import gtk.glade
 
+Settings = {}
+
 class gui(gtk.glade.XML):
     def __init__(self):
+	w = lambda x: self.wTree.get_widget(x)
+
 	self.local_path = os.path.realpath(os.path.dirname(sys.argv[0]))
 	gladefile = os.path.join(self.local_path, "iriq.glade")
 	windowname = "MainWindow"
 	self.wTree = gtk.glade.XML(gladefile)
 	self.wTree.signal_autoconnect(self)
-	self.main_window = self.wTree.get_widget("MainWindow")
-	self.main_window.set_title("Blah")
-	self.main_notebook = self.wTree.get_widget("MainNotebook")
-	print self.main_notebook.get_current_page()
-	self.applabel = self.wTree.get_widget("AppLabel")
-	print self.applabel
-	self.applabel = self.wTree.get_widget("HotkeysLabel")
-	print self.applabel
-	self.applabel = self.wTree.get_widget("RulesLabel")
-	print self.applabel
-	self.main_notebook.set_current_page(0)
+
+	# Fill the gui
+	w("MainWindow").set_title("Blah")
+	w("DecTiledBtn").set_active(int(Settings["Echinus*decoratetiled"]))
+	w("ShowTagBarBtn").set_active(int(Settings["Echinus*tagbar"]))
+
     def window_destroyed(self, widget):
 	gtk.main_quit()
 
+class parser(file):
+    def __init__(self, fname):
+	f = open(fname, mode='r+')
+	self.read(f)
+
+    def read(self, f):
+	for line in f.readlines():
+	    try:
+		l = line.split(':')
+		Settings[l[0]] = l[1]
+	    except:
+		pass
+	print Settings
+
 if __name__ == "__main__":
-    app = gui()
-    gtk.main()
+	#par = parser(sys.argv[1])
+	par = parser("echinusrc")
+	app = gui()
+	gtk.main()
